53417659d06a3d037c67a33bee6f93fcf1d2b624
Ralph Julian Siasat <rsiasat00@gmail.com>
2020-03-03 20:40:08 +0800

    first commit

---
+++ src/components/Table.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <table id="dataTable" class="table">
+      <thead>
+        <tr>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
+
+<script>
+import $ from "jquery";
+import "datatables.net-bs4";
+import "datatables.net-buttons-bs4";
+
+export default {
+  mounted() {
+    $("#dataTable").DataTable({
+      processing: true,
+      serverSide: true,
+      paging: true,
+      ajax: (process.env.VUE_APP_API_URL ? process.env.VUE_APP_API_URL : "http://127.0.0.1:8000") + "/api/users"
+    });
+  }
+};
+</script>
+
+<style>
+#dataTable {
+  text-align: left;
+  margin: 1rem;
+}
+</style>
